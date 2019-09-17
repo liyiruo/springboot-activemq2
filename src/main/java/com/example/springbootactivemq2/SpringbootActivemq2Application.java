74631a51861055f0119c69386ef53361407c4e7f
李依若 <401213245@qq.com>
2019-09-17 17:46:31 +0800

    漏掉了一个文件

---
+++ src/main/java/com/example/springbootactivemq2/SpringbootActivemq2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootactivemq2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActivemq2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActivemq2Application.class, args);
+    }
+
+}
